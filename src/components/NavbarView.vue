5bbbef013abb9fd9eb4995ec71e6da639a4bdc8d
jorgeurrutia21 <60193767+jorgeurrutia21@users.noreply.github.com>
2026-05-21 16:54:13 -0400

    21-05-26
    se crea la carpeta components donde se utilizó  vue 3 que contiene los siguientes componentes y su modificación , siendo la principal característica la separación del footer y el navbar.
    
    -navbar  se modifico la posición de la barra de búsqueda , quedando sobre las card
    
    -footer se mantuvo el estilo y su diseño ajustando pequeños detalles

---
+++ src/components/NavbarView.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <div class="container-fluid">
+            <router-link to="/" class="navbar-brand fw-bold">
+                🌤 El Tiempo
+            </router-link>
+
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#menuNav"
+                aria-controls="menuNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menuNav">
+                <div class="navbar-nav ms-auto align-items-center">
+                    <router-link to="/" class="nav-link"> Home </router-link>
+
+                    <router-link to="/about" class="nav-link">
+                        Nosotros
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.navbar {
+    background-color: #071629;
+    color: #ffffff;
+    padding: 1rem 0;
+}
+</style>
